9b7638e11bd13367d36a364424a8b090b54435b6
Emil-18 <emilhesmyr@icloud.com>
2025-11-27 22:50:10 +0100

    Revert "Displays all the posts of all users"
    This reverts commit c0b0a653ffb7df093fc10a35d77003c5a64c4f42.

--- app/(tabs)/home.tsx
+++ app/(tabs)/home.tsx
@@ -1,46 +1,19 @@
-import React, { useEffect, useState } from "react";
-import { FlatList, View, Text, Image } from "react-native";
 import Button from "@/components/Button";
+import React from "react";
+import { View } from "react-native";
 import { styles } from "../styles";
 
-import { db } from "../../FirebaseConfig";
-import { collection, getDocs } from "firebase/firestore";
-
-interface Post {
+interface Pearl {
   id: string;
   title: string;
-  description: string;
-  imageUrls?: string[];
-  author: {
-    uid: string;
-    displayName: string | null;
-    photoURL: string | null;
-  };
 }
 
-const Home = () => {
-  const [posts, setPosts] = useState<Post[]>([]);
+const placeHolderPearls = [
+  { id: "1", title: "pearl1" },
+  { id: "2", title: "pearl2" },
+];
 
-  useEffect(() => {
-    const loadPosts = async () => {
-      const postsCol = collection(db, "posts");
-      const snapshot = await getDocs(postsCol);
-
-      const results: Post[] = [];
-
-      snapshot.forEach((doc) => {
-        results.push({
-          id: doc.id,
-          ...doc.data(),
-        } as Post);
-      });
-
-      setPosts(results);
-    };
-
-    loadPosts();
-  }, []);
-
+const home = () => {
   return (
     <View>
       <Button
@@ -49,41 +22,14 @@ const Home = () => {
         buttonStyle={styles.profileButton}
         buttonTextStyle={styles.profileText}
       />
-
       <Button
         text="Innstillinger"
         path="/settings"
         buttonStyle={styles.profileButton}
         buttonTextStyle={styles.profileText}
       />
-
-      <Text style={{ marginTop: 20, fontSize: 20, fontWeight: "bold" }}>
-        Innlegg fra alle brukere
-      </Text>
-
-      <FlatList
-        data={posts}
-        keyExtractor={(item) => item.id}
-        renderItem={({ item }) => (
-          <View style={{ marginVertical: 15 }}>
-            <Text style={{ fontWeight: "bold" }}>{item.title}</Text>
-            <Text>{item.description}</Text>
-
-            {item.imageUrls && item.imageUrls.length > 0 && (
-              <Image
-                source={{ uri: item.imageUrls[0] }}
-                style={{ width: 150, height: 150, marginTop: 10 }}
-              />
-            )}
-
-            <Text style={{ marginTop: 5, fontStyle: "italic" }}>
-              Av: {item.author.displayName ?? "Ukjent bruker"}
-            </Text>
-          </View>
-        )}
-      />
     </View>
   );
 };
 
-export default Home;
+export default home;
